cc5f833357a9dfce55c2d8254eff2ff9c01e4389
Telmo Alexandre <telmo93@gmail.com>
2018-05-10 21:13:31 +0100

    Desktop old changes.
    Commit to allow a merge.

--- WebNews_19089/WebNews_19089/App_Start/RouteConfig.cs
+++ WebNews_19089/WebNews_19089/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace WebNews_19089 {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "News", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
