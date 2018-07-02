66a7a585ab40040c34fc3e3d8b58ae1a088d0d7b
Telmo Alexandre <telmo93@gmail.com>
2018-07-02 17:45:51 +0100

    Já é possivel comentar nas noticias.
    Criada Partial View que contém form que permite inserir novos comentários.
    Criado um controller que gere os POST dos novos comentários.
    Noticias mostram 'No comments' quando não existem comentários.
    Melhoramentos CSS.

---
+++ WebNews_19089/WebNews_19089/Controllers/CommentsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebNews_19089.Models;
+
+namespace WebNews_19089.Controllers
+{
+    public class CommentsController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        public CommentsController()
+        {
+        }
+
+        // GET: Comments
+        [HttpPost]
+        [ValidateAntiForgeryToken]        
+        public ActionResult Create([Bind(Include = "Content, NewsFK")] Comments comment, string email)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                var user = db.UsersProfile.Where(u => u.UserName.Equals(email)).First();
+
+                comment.CommentDate = DateTime.Now;
+
+                comment.UserProfileFK = user.ID;
+
+                db.Comments.Add(comment);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Details", "News", new { id = comment.NewsFK });
+        }
+    }
+}
