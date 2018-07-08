f749c153968b535230863ded4c8648ee5db9c472
Telmo Alexandre <telmo93@gmail.com>
2018-07-08 17:50:33 +0100

    Erros aplicados no CommentsController.

--- WebNews_19089/WebNews_19089/Controllers/CommentsController.cs
+++ WebNews_19089/WebNews_19089/Controllers/CommentsController.cs
@@ -18,47 +18,53 @@ namespace WebNews_19089.Controllers
 
 
         // GET: ~/Comments/Edit/{id, email}
-        public ActionResult Edit(int? id, string email, string Page)
+        public ActionResult Edit(int? id, string Page)
         {
 
-            if (id != null)
+            if (id == null)
             {
-
-                Comments comment = db.Comments.Find(id);
-
-                // Caso tenha encontrado o comentário
-                if (comment != null)
-                {
-
-                    // Garantir que o utilizador está autenticado
-                    if (email != null)
-                    {
-                        // Garantir que se trata do dono do comentario
-                        if (email.Equals(comment.UserProfile.UserName))
-                        {
-
-                            comment.Content = comment.Content.Replace("<br/>", "\r\n");
-
-                            // Guardar a pagina de origem deste GET para retornar corretamente no DeleteConfirm
-                            // Criei um view model que recebe um objeto Comments e uma string com a página de origem para este GET
-                            return View(new CommentsModificationViewModel
-                            {
-                                comment = comment,
-                                Page = Page
-                            });
-                        }
-                    }
-                    else
-                    {
-                        // Se não estiver logado
-                        // Redirecionar para o login
-                        return RedirectToAction("Login", "Account", null);
-                    }
-                }
-
+                return RedirectToAction("Index", "UserError", new { error = "ID not provided." });
             }
 
-            return RedirectToAction("Index", "News", null);
+            Comments comment = db.Comments.Find(id);
+
+            // Caso tenha não encontrado o comentário
+            if (comment == null)
+            {
+                return RedirectToAction("Index", "UserError", new { error = "We couldn't find this comment.", details = "The ID was not valid." });
+            }
+
+            // Garantir que o utilizador está autenticado
+            if (User.Identity.Name != null)
+            {
+                // Garantir que se trata do dono do comentario
+                if (User.Identity.Name == comment.UserProfile.UserName)
+                {
+
+                    comment.Content = comment.Content.Replace("<br/>", "\r\n");
+
+                    // Guardar a pagina de origem deste GET para retornar corretamente no DeleteConfirm
+                    // Criei um view model que recebe um objeto Comments e uma string com a página de origem para este GET
+                    return View(new CommentsModificationViewModel
+                    {
+                        comment = comment,
+                        // Para saber a localização de origim do pedido para poder fazer o redirect
+                        // correto no POST
+                        Page = Page
+                    });
+                }
+                else
+                {
+                    return RedirectToAction("Index", "UserError", new { error = "You can't edit this comment.", details = "You lack the permissions to do so." });
+                }
+            }
+            else
+            {
+                // Se não estiver logado
+                // Redirecionar para o login
+                return RedirectToAction("Login", "Account", null);
+            }
+
         }
 
 
@@ -67,78 +73,86 @@ namespace WebNews_19089.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "ID,NewsFK,UserProfileFK,CommentDate,Content")] Comments comment, string Page)
         {
-                if (ModelState.IsValid)
+            if (ModelState.IsValid)
+            {
+
+                comment.Content = comment.Content.Replace("\r\n", "<br/>");
+
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+
+                // Retornar para a página em que foi originado o pedido GET
+                if (Page == "Manage")
                 {
-
-                    comment.Content = comment.Content.Replace("\r\n", "<br/>");
-
-                    db.Entry(comment).State = EntityState.Modified;
-                    db.SaveChanges();
-
-                    // Retornar para a página em que foi originado o pedido GET
-                    if (Page == "Manage")
-                    {
-                        return RedirectToAction("Index", "Manage", new { email = User.Identity.Name });
-                    }
-                    else
-                    {
-                        return RedirectToAction("Details", "News", new { id = comment.NewsFK });
-                    }
+                    return RedirectToAction("Index", "Manage", new { email = User.Identity.Name });
                 }
                 else
                 {
-                    // Retornar com a validation message
-                    return View(new CommentsModificationViewModel {
-                        comment = comment,
-                        Page = Page
-                    });
+                    return RedirectToAction("Details", "News", new { id = comment.NewsFK });
                 }
+            }
+            else
+            {
+                // Retornar com a validation message
+                return View(new CommentsModificationViewModel
+                {
+                    comment = comment,
+                    // Para saber a localização de origim do pedido para poder fazer o redirect
+                    // correto no POST
+                    Page = Page
+                });
+            }
         }
 
         // Este GET do delete tem autenticação manual feita por mim
         // Para garantir que o dono do comentário o pode apagar.
 
         // GET: ~/Comments/Delete/{id, email}
-        public ActionResult Delete(int? id, string email, string Page)
+        public ActionResult Delete(int? id, string Page)
         {
 
-            if (id != null)
+            if (id == null)
             {
-
-                Comments comment = db.Comments.Find(id);
-
-                // Caso tenha encontrado o comentário
-                if (comment != null)
-                {
-
-                    // Garantir autenticação manualmente para garantir que o user tem o role necessário
-                    // ou é o dono do comentário
-                    if (email != null)
-                    {
-                        // Garantir que se trata do dono do comentario, ou de um utilizador com o papel necessario para apagar o mesmo
-                        if (email.Equals(comment.UserProfile.UserName) || User.IsInRole("Admin") || User.IsInRole("NewsEditor"))
-                        {
-
-                            // Guardar a pagina de origem deste GET para retornar corretamente no DeleteConfirm
-                            // Criei um view model que recebe um objeto Comments e uma string com a página de origem para este GET
-                            return View(new CommentsModificationViewModel
-                            {
-                                comment = comment,
-                                Page = Page
-                            });
-                        }
-                    }
-                    else
-                    {
-                        // Se não tiver autenticado(email == null)
-                        // Redirecionar para o login
-                        return RedirectToAction("Login", "Account", null);
-                    }
-                }
-
+                return RedirectToAction("Index", "UserError", new { error = "ID not provided." });
             }
 
-            return RedirectToAction("Index", "News", null);
+            Comments comment = db.Comments.Find(id);
+
+            // Caso tenha encontrado o comentário
+            if (comment == null)
+            {
+                return RedirectToAction("Index", "UserError", new { error = "We couldn't find this comment.", details = "The ID was not valid." });
+            }
+
+            // Garantir autenticação manualmente para garantir que o user tem o role necessário
+            // ou é o dono do comentário
+            if (User.Identity.Name != null)
+            {
+                // Garantir que se trata do dono do comentario, ou de um utilizador com o papel necessario para apagar o mesmo
+                if (User.Identity.Name == comment.UserProfile.UserName || User.IsInRole("Admin") || User.IsInRole("NewsEditor"))
+                {
+
+                    // Guardar a pagina de origem deste GET para retornar corretamente no DeleteConfirm
+                    // Criei um view model que recebe um objeto Comments e uma string com a página de origem para este GET
+                    return View(new CommentsModificationViewModel
+                    {
+                        comment = comment,
+                        // Para saber a localização de origim do pedido para poder fazer o redirect
+                        // correto no POST
+                        Page = Page
+                    });
+                }
+                else
+                {
+                    return RedirectToAction("Index", "UserError", new { error = "You can't delete this comment.", details = "You lack the permissions to do so." });
+                }
+            }
+            else
+            {
+                // Se não tiver autenticado(email == null)
+                // Redirecionar para o login
+                return RedirectToAction("Login", "Account", null);
+            }
         }
 
         // POST: ~/Comments/Delete/{id, email}
