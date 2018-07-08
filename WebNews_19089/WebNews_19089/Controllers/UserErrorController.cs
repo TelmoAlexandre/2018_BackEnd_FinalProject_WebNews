f401a704d4b0b99931f9dbabc0912c8d7fe64e8a
Telmo Alexandre <telmo93@gmail.com>
2018-07-08 16:34:45 +0100

    Criada uma view para mostrar erros ao utilizador.
    Criado um controller UserError que contem um Index com 2 parametros que irá chamar a view e mostrar o erro.

---
+++ WebNews_19089/WebNews_19089/Controllers/UserErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebNews_19089.Models;
+
+namespace WebNews_19089.Controllers
+{
+    public class UserErrorController : Controller
+    {
+        // GET: UserError
+        public ActionResult Index(string error, string details)
+        {
+            // Verificar se recebeu o erro
+            if(error != null)
+            {
+                return View(new UserErrorModelView {
+                    Error = error,
+                    AdditionalDetails = details
+                });
+            }
+            else
+            {
+                // Caso não tenha recebido um erro, criar um erro genérico
+                return View(new UserErrorModelView
+                {
+                    Error = "Something went wrong.",
+                    AdditionalDetails = null
+                });
+            }
+        }
+    }
+}
---
+++ WebNews_19089/WebNews_19089/Models/UserErrorModelView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebNews_19089.Models
+{
+    public class UserErrorModelView
+    {
+
+        public string Error { get; set; }
+        public string AdditionalDetails { get; set; }
+    }
+}
